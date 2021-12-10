570eb88bafc0ae13150e6f14c16cf845e8aed7b6
Caitlin Tan <ppcctan@gmail.com>
2021-12-10 15:46:00 -0800

    added new line, fixed syntax to compile

--- Final/q5.asm
+++ Final/q5.asm
@@ -6,7 +6,8 @@
 # num through syscall
 
     .data
-prompt" .asciiz "Type three numbers:"
+prompt: .asciiz "Type three numbers:"
+nl: .asciiz "\n"
 
     .text
     .globl main
@@ -22,18 +23,19 @@ ori $s2, $s2, 10            # set s2 as constant, which will be used in multipli
 lui	$t0, 0xffff		        # load address of kernel
 
 input:
-lw	$t1, 0($t0)		        # load receiver control
+lw	    $t1, 0($t0)		    # load receiver control
 andi	$t1, $t1, 0x0001	# clear all except lowest bit
 beqz	$t1, input          # loop if not done yet
 nop
 
 lw      $t3, 4($t0)         # take value of transmitter 
-mult    $s1, $s1, $s2       # value *= 10
+mult    $s1, $s2            # value *= 10
 mflo    $s1                 # move value to 10
 addu    $s1, $s1, $t3       # saved value + inputted value
 addi    $s0, $s0, -1        # counter--
 
 bgtz    $s0, input          # if(counter > 0), branch to input
+nop
 
 li      $v0, 1              # print three digit number
 move    $a0, $s0
