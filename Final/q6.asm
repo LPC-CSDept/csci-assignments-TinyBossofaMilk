1cf4d2fcedd105ed56b93c5577b2e530f6e0e048
Caitlin Tan <ppcctan@gmail.com>
2021-12-10 13:06:05 -0800

    first commit

---
+++ Final/q6.asm
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+
+Make the assembly program with Interrupted I/O.
+
+Take the user character and print it in a console until 'q' is typed.
+Make the interrupt handler(Kernel text program)
+ 
+
+Make the file in any editor that supports the Git and then commit/push to https://classroom.github.com/a/rz6NTjtc (Links to an external site.) 
+
+You can use the browser version of VS code (https://vscode.dev (Links to an external site.)).
+
+file name: q6.asm
+You should commit and push your file every 5 min.
+Attach the screen result to this Canvas.
+Elaborate on your program source code.
+how the interrupt could be enabled
+What happened when the user type a character at the keyboard
+how your program notice the interrupt occurs
+how do we figure out the exception type
+Before we return to the user program, what should the kernel program do?
+ 
