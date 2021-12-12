51d23c58d7390e4334e2784be9bfa59b9cea4c24
Caitlin Tan <ppcctan@gmail.com>
2021-12-12 00:05:01 -0800

    edits with loop and kernel

--- Final/q6.asm
+++ Final/q6.asm
@@ -26,10 +26,7 @@ lw	    $t0, 0($s0)		    # load receiver control
 andi	$t0, $t0, 0x0001	# clear all except lowest bit
 beqz	$t0, inputloop      # loop if not done yet
 nop
-lw      $s1, 4($s0)         # save input to s1
-j       check
-nop
-
+lw      $s1, 4($s0)         # save input to $s1
 
 printloop:                  # assumes $t0 is already set
 lw      $t0, 8($s0)         # load transmitter control
@@ -37,21 +34,13 @@ andi    $t0, $t0, 0x0001    # select LSB
 bnez    $t0, printloop      # wait till transmitter control is ready
 nop
 
-sw      $s0,  12($s0)       # write to transmitter data
+sw      $s1,  12($s0)       # write to transmitter data
 b       inputloop           # loop back to input loop
 nop
 
 quit:
 li      $v0, 10
 syscall
-
-
-#############we do not ddo manual checks, will be in kernel
-check:
-addi    $t0, $s1, -113      # char - 113 ('q')
-beqz    $t0, quit           # if inputted char is 'q', quit
-nop
-##################################
 
 # KERNEL text ******************
 
@@ -62,29 +51,8 @@ srl     $a0, $k0, 2     #   Extract   ExcCode     Field
 andi    $a0, $a0, 0x1f  #   Get the exception code     
 bne     $a0, $zero,   kdone     # Exception Code 0 is I/O. Only processing I/O here     
 
+
+# implement quit in kernel
 addi    $t0, $s1, -113      # char - 113 ('q')
 beqz    $t0, quit           # if inputted char is 'q', quit
 nop
-
-
-Make the assembly program with Interrupted I/O.
-
-Take the user character and print it in a console until 'q' is typed.
-Make the interrupt handler(Kernel text program)
- 
-
-Make the file in any editor that supports the Git and then commit/push to https://classroom.github.com/a/rz6NTjtc (Links to an external site.) 
-
-You can use the browser version of VS code (https://vscode.dev (Links to an external site.)).
-
-file name: q6.asm
-You should commit and push your file every 5 min.
-Attach the screen result to this Canvas.
-Elaborate on your program source code.
-how the interrupt could be enabled
-What happened when the user type a character at the keyboard
-how your program notice the interrupt occurs
-how do we figure out the exception type
-Before we return to the user program, what should the kernel program do?
- 
-
